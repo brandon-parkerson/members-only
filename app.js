953855edd5a991ecb38f948e7a259e86b0f6340c
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-04-29 20:35:44 +0300

    set up routes and controller for index

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const session = require("express-session");
 const passport = require("passport");
 const localStrategy = require("passport-local");
-
+const usersRouter = require("./routes/usersRouter")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -13,8 +13,6 @@ app.use(session({secret: "osmo", resave: false, saveUninitialized: false}));
 app.use(passport.session());
 app.use(express.urlencoded({extended: false}));
 
-app.get("/", (req, res) => {
-    res.render("index");
-})
+app.use("/", usersRouter);
 
 app.listen(3000, () => console.log("app listening on port 3000"));
