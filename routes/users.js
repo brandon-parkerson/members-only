2f2b6b59ffa7375b65436557a7197028dd4906c6
Brandon Parkerson <bpkp1996@Osmo.>
2025-06-21 20:49:51 +0300

    more routing and forms

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
-
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 // routes
 const usersRouter = require("./routes/users");
 
+
 app.use("/users", express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+// serve static files like css
+app.use(express.static("public"));
 
 app.use("/", usersRouter);
+app.use("/register", usersRouter);
 
 app.listen(3000);
