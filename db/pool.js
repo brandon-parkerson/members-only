e859f87ebc4dcd49b2911fb4a971e1b8eb383d4e
Brandon Parkerson <bpkp1996@Osmo.>
2025-06-27 17:31:07 +0300

    db pool

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
-module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT, // The default port
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+module.exports = pool;
